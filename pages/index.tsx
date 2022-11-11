b057c5f50c9898178b4d045c5aea8f7925eaf24b
Damo Softwares <73931600+Dhamo2005@users.noreply.github.com>
2022-11-11 01:00:31 +0000

    c

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Damo Softwares | Home Page</title>
+        <title>Damo Softwares Dhamo | Home Page</title>
         <meta name="description" content="Your desired choice" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
