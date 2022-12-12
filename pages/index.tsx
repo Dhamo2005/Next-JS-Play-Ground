562cc761a7c992d16c061433c1e5fbc808a06e63
Damo Softwares <73931600+Dhamo2005@users.noreply.github.com>
2022-12-12 02:40:58 +0000

    c

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import Header from '../comoponents/appbar'
 function Home() {
   return (
     <div className={styles.container}>
@@ -9,7 +9,7 @@ function Home() {
         <meta name="description" content="Your desired choice" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+        <Header />
       <main className={styles.main}>
         Damo Softwares 
       </main>
