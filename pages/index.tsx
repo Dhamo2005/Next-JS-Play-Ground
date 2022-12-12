94a090f0cb35730cce3cfeb17f71025c53a175c4
Damo Softwares <73931600+Dhamo2005@users.noreply.github.com>
2022-12-12 02:42:46 +0000

    c

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Header from '../comoponents/appbar'
 function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Damo Softwares Dhamo | Home Page</title>
         <meta name="description" content="Your desired choice" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <Header />
-      <main className={styles.main}>
+      <main>
         Damo Softwares 
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         Footer
       </footer>
     </div>
